afb476f087bc6eaf86b132a32926f9eb166863ce
Pavel Loginov <ploginoff@gmail.com>
2016-10-28 10:14:43 +0300

    Init

---
+++ calc.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var first, second, start, max, i, count, now, result uint64
+
+	first = 36
+	second = 18
+	start = 1<<second - 1
+	max = (1<<first - 1) &^ (1<<(first-second) - 1)
+
+	fmt.Printf("%b\n", start)
+	fmt.Printf("%b\n", max)
+
+	for i = start; i <= max; i++ {
+		now = i
+		count = 0
+		for now != 0 {
+			if now&1 == 1 {
+				count++
+			}
+			now >>= 1
+		}
+		if count == second {
+			result++
+		}
+	}
+
+	fmt.Println(result)
+}
